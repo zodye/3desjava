8b90e61dcde193d99ba96690b6cd51c01be24cf5
Jodie Anderson <38399103+zodye@users.noreply.github.com>
2020-06-13 21:29:25 +0700

    Update TripleDESenkripsi.java

--- TripleDESenkripsi.java
+++ TripleDESenkripsi.java
@@ -8,15 +8,25 @@ public class TripleDESenkripsi {
 		System.out.print("Masukkan teks: ");
 		Scanner in = new Scanner(System.in);
 		String input = in.nextLine();
+		String enc1,enc2,enc3;
 		
-		System.out.print("Kunci: ");
+		System.out.print("Kunci 1: ");
 		String inKey = in.nextLine();
+		System.out.print("Kunci 2: ");
+		String inKey2 = in.nextLine();
+		System.out.print("Kunci 3: ");
+		String inKey3 = in.nextLine();
 		in.close();
-		TripleDES tes = new TripleDES(inKey);
 		try{
-			System.out.println("Hasil: "+tes.harden(input));
+			TripleDES tes1 = new TripleDES(inKey);
+			enc1=tes1.encrypt(input);
+			TripleDES tes2 = new TripleDES(inKey2);
+			enc2=tes2.encrypt(enc1);
+			TripleDES tes3 = new TripleDES(inKey3);
+			enc3=tes3.encrypt(enc2);
+			System.out.println("Hasil enkripsi: "+enc3);
 		}catch(Exception e) {
-			
+			System.out.println(e);
 		}
 	}
 }
