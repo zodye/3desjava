937616ac479f281130e293c1bcecbd498dff6596
Jodie Anderson <38399103+zodye@users.noreply.github.com>
2020-06-16 10:08:23 +0700

    Update TripleDESenkripsi.java

--- TripleDESenkripsi.java
+++ TripleDESenkripsi.java
@@ -12,11 +12,15 @@ public class TripleDESenkripsi {
 		System.out.print("Kunci: ");
 		String inKey = in.nextLine();
 		in.close();
-		try{
-			TripleDES tes = new TripleDES(inKey);
-			System.out.println("Hasil enkripsi: "+tes.encrypt(input));
-		}catch(Exception e) {
-			System.out.println(e);
+		if(inKey.length()>=22){
+			try{
+				TripleDES tes = new TripleDES(inKey);
+				System.out.println("Hasil enkripsi: "+tes.encrypt(input));
+			}catch(Exception e) {
+				System.out.println(e);
+			}
+		}else{
+			System.out.println("Harap masukkan kunci minimal 22 karakter.");
 		}
 	}
 }
